418007992e86e8b98905fa9004bea5d0c1efd6df
Hank O'Brien <hjobrien@berkeley.edu>
2020-06-29 11:10:37 -0700

    Added React and Redux devtools

--- electron/main.js
+++ electron/main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const url = require('url');
 const { channels } = require('../src/shared/constants');
+const { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } = require('electron-devtools-installer');
 
 let mainWindow;
 
@@ -20,8 +21,8 @@ function createWindow () {
     });
     mainWindow.loadURL(startUrl);
     mainWindow.on('closed', function () {
-    mainWindow = null;
-});
+        mainWindow = null;
+    });
 }
 
 app.on('ready', createWindow);
@@ -34,6 +35,17 @@ app.on('activate', function () {
 if (mainWindow === null) {
     createWindow();
 }
+});
+
+app.whenReady().then(() => {
+    installExtension(REACT_DEVELOPER_TOOLS)
+        .then((name) => console.log(`Added Extension:  ${name}`))
+        .catch((err) => console.log('An error occurred: ', err));
+});
+app.whenReady().then(() => {
+    installExtension(REDUX_DEVTOOLS)
+        .then((name) => console.log(`Added Extension:  ${name}`))
+        .catch((err) => console.log('An error occurred: ', err));
 });
 
 ipcMain.on(channels.APP_INFO, (event) => {
--- src/react/Store.js
+++ src/react/Store.js
@@ -1,6 +1,8 @@
-import { createConnectedStore } from 'undux'
+import { createConnectedStore, withReduxDevtools} from 'undux'
+
 
 // Create a store with an initial value.
 export default createConnectedStore({
     showAddRecipeScreen: false,
-  })
+    recipeListStale: false,
+  }, withReduxDevtools)
